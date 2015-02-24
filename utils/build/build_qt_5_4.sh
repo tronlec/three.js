6b0f742549a01f058542c2de01f87cf8f932e724
Pasi Keranen <pasi.keranen@digia.com>
2015-02-24 09:09:37 +0200

    Added missing build_qt_5_4.sh file.

---
+++ utils/build/build_qt_5_4.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Build script for Qt 5.4.x environments without the native TypedArray support.
+# Works only with Qt Canvas3D Technology Preview 2.0 release available from:
+#
+cd "$(dirname "$0")"
+python build.py --include qt_5_4 --include extras --output ../../build/three.js
+python build.py --include extras --externs externs/extras.js --output ../../build/three-extras.js
+python build.py --include math --output ../../build/three-math.js
