4da35847ad48b6fcaee0fe8c425a4ce40f90256b
Pasi Keranen <pasi.keranen@digia.com>
2015-04-01 15:12:14 +0300

    Removed Qt 5.4, three-extras and three-math build files.
    Removed the Qt 5.4 related files as those are not enough to support Qt 5.4 in this R71 port.
    Also removed the build files for three-extras and three-math as those are not built by the
    R71 upstream.

--- utils/build/build_qt_5_5.sh
+++ utils/build/build_qt_5_5.sh
@@ -5,5 +5,5 @@
 cd "$(dirname "$0")"
 python build.py --include qt_5_5 --include common --include extras --output ../../build/three.js
 #python build.py --include qml --include common --include extras --minify --output ../../build/three.min.js
-python build.py --include extras --externs externs/extras.js --output ../../build/three-extras.js
-python build.py --include math --output ../../build/three-math.js
+#python build.py --include extras --externs externs/extras.js --output ../../build/three-extras.js
+#python build.py --include math --output ../../build/three-math.js
