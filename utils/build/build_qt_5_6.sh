c6ef5966080c7382846872323dda267376de11a5
Miikka Heikkinen <miikka.heikkinen@theqtcompany.com>
2015-08-31 12:26:45 +0300

    Move the license header back to the beginning of the generated three.js

--- utils/build/build_qt_5_6.sh
+++ utils/build/build_qt_5_6.sh
@@ -2,4 +2,4 @@
 #Build script for Qt 5.6 environments that offer native TypedArray support
 #Works only with Qt Canvas3D 1.1 and later.
 cd "$(dirname "$0")"
-python build.py --include common --include extras --include qt_5_6 --output ../../build/three.js
+python build.py --include qt_license --include common --include extras --include qt_5_6 --output ../../build/three.js
